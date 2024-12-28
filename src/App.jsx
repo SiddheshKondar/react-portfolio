8b3c5b60bd7aee51ce22a381b5ca4cfad69bddb5
SiddheshKondar <siddheshkondar20@gmail.com>
2024-12-28 18:45:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,23 @@ import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Projects } from "./components/Projects/Projects";
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
+      <BrowserRouter basename="/react-portfolio">
       <Navbar />
       <Hero />
       <About />
       {/*<Experience /> */}
       <Projects />
       <Contact />
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+
